c2a93168725b1f32124419786b00e758352a19b3
YuanCoder <1073625256@qq.com>
2017-06-13 23:37:02 +0800

    项目整体框架的搭建

---
+++ src/main/java/com/yuan/engine/SearchBlogEngineApplication.java
@@ -0,0 +1,12 @@
+package com.yuan.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchBlogEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchBlogEngineApplication.class, args);
+	}
+}
---
+++ src/main/java/com/yuan/engine/entity/User.java
@@ -0,0 +1,26 @@
+package com.yuan.engine.entity;
+
+/**
+ * Created by Administrator on 2017/6/13.
+ */
+public class User {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
